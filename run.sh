eeece8a37347a78e94c426b492f4620134f8ac9a
haidangcap1 <92019629+haidangcap1@users.noreply.github.com>
2021-11-21 07:52:01 +0000

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+!apt updae
+!apt install selenium
+!pip install - r requirements.txt 
+!pip install selenium
+!apt install chromium-chromedriver
+!cp /usr/lib/chromium-browser/chromedriver /usr/bin
